a8cdbd08764b36facc0f3d2a344bcc5c673e8107
AkashKannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-05-08 20:16:13 +0530

    Initialize Spring Boot project for GateKeeping app with Web, JPA, MySQL, Validation, and Lombok

---
+++ src/main/java/com/akashkannapiran/GateKeeping/GateKeepingApplication.java
@@ -0,0 +1,13 @@
+package com.akashkannapiran.GateKeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateKeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateKeepingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akashkannapiran/GateKeeping/GateKeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashkannapiran.GateKeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateKeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
